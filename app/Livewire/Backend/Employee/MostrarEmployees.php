b86554e6dab2943a416300d03e287285a28e22dd
Jorge <jorgedanielpacosillo@gmail.com>
2024-11-10 15:26:41 -0400

    funcionalidad implementada de borrar un empleado y su imagen asociada al empĺeado

--- app/Livewire/Backend/Employee/MostrarEmployees.php
+++ app/Livewire/Backend/Employee/MostrarEmployees.php
@@ -2,24 +2,44 @@
 
 namespace App\Livewire\Backend\Employee;
 
+use App\Models\Page;
 use App\Models\Section;
 use Livewire\Component;
 use App\Models\Employee;
-use App\Models\Page;
+use Illuminate\Support\Facades\Storage;
 use Livewire\WithPagination;
 
 class MostrarEmployees extends Component
 {
+
+
+    protected $listeners = ['eliminarEmployee'];
+
     use WithPagination;
 
     public $section;
     public $page;
+
 
     public function mount(Section $section, Page $page)
     {
         $this->section = $section;
         $this->page = $page;
     }
+
+    // #[On('eliminarEmployee'),section]
+    public function eliminarEmployee(Employee $employee, Section $section)
+    {
+        // Verificar si el empleado tiene una imagen
+        if ($employee->image && Storage::disk('public')->exists('employees/' . $employee->image)) {
+            // Eliminar la imagen del almacenamiento
+            Storage::disk('public')->delete('employees/' . $employee->image);
+        }
+
+        // Eliminar el registro del empleado
+        $employee->delete();
+    }
+
 
     public function render()
     {
--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -10,76 +10,77 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap5.min.js"></script>
+    @livewireStyles
 </head>
 
 <body class="flex h-full demo1 sidebar-fixed header-fixed bg-[#fefefe] dark:bg-coal-500">
-<script src="{{ asset('assetsBackend/js/login/utilitis.js') }}"></script>
-<style>
-
-    /* From Uiverse.io by vikramsinghnegi */
-    .blob {
-        width: 112px;
-        height: 112px;
-        display: grid;
-        background: #fff;
-        filter: blur(5.6px) contrast(10);
-        padding: 11.2px;
-        mix-blend-mode: darken;
-    }
-
-    .blob:before,
-    .blob:after {
-        content: "";
-        grid-area: 1/1;
-        width: 44.8px;
-        height: 44.8px;
-        background: rgb(177, 42, 56);
-        animation: blob-rhf26m 2s infinite;
-    }
-
-    .blob:after {
-        animation-delay: -1s;
-    }
-
-    @keyframes blob-rhf26m {
-        0% {
-            transform: translate(0, 0);
+    <script src="{{ asset('assetsBackend/js/login/utilitis.js') }}"></script>
+    <style>
+        /* From Uiverse.io by vikramsinghnegi */
+        .blob {
+            width: 112px;
+            height: 112px;
+            display: grid;
+            background: #fff;
+            filter: blur(5.6px) contrast(10);
+            padding: 11.2px;
+            mix-blend-mode: darken;
         }
 
-        25% {
-            transform: translate(100%, 0);
+        .blob:before,
+        .blob:after {
+            content: "";
+            grid-area: 1/1;
+            width: 44.8px;
+            height: 44.8px;
+            background: rgb(177, 42, 56);
+            animation: blob-rhf26m 2s infinite;
         }
 
-        50% {
-            transform: translate(100%, 100%);
+        .blob:after {
+            animation-delay: -1s;
         }
 
-        75% {
-            transform: translate(0, 100%);
-        }
+        @keyframes blob-rhf26m {
+            0% {
+                transform: translate(0, 0);
+            }
 
-        100% {
-            transform: translate(0, 0);
-        }
-    }
+            25% {
+                transform: translate(100%, 0);
+            }
 
-</style>
-<div id="cargando-overlay" class="overlay">
-    <div class="loader">
-        <div class="blob"></div>
+            50% {
+                transform: translate(100%, 100%);
+            }
+
+            75% {
+                transform: translate(0, 100%);
+            }
+
+            100% {
+                transform: translate(0, 0);
+            }
+        }
+    </style>
+    <div id="cargando-overlay" class="overlay">
+        <div class="loader">
+            <div class="blob"></div>
+        </div>
     </div>
-</div>
 
 
-<div class="flex grow">
-    @include('backend.partials.sidebar')
-    <div class="wrapper flex grow flex-col">
-        @include('backend.partials.header')
-        @yield('content')
-        @include('backend.partials.footer')
+    <div class="flex grow">
+        @include('backend.partials.sidebar')
+        <div class="wrapper flex grow flex-col">
+            @include('backend.partials.header')
+            @yield('content')
+            @include('backend.partials.footer')
+        </div>
     </div>
-</div>
-@include('backend.layouts.scrips')
+    @livewireScripts
+    @include('backend.layouts.scrips')
+    @stack('scripts-backend-employees')
 </body>
 
 </html>
